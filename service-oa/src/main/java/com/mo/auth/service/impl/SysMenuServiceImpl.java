f53269aac11ee4a09217afd0f31c9c01e59e6178
mozihao_glb <mozihao_glb@qq.com>
2023-10-09 10:28:24 +0800

    实现登录成功后返回的信息接口，包含可操作的菜单按钮、及其他基本信息

--- service-oa/src/main/java/com/mo/auth/service/impl/SysMenuServiceImpl.java
+++ service-oa/src/main/java/com/mo/auth/service/impl/SysMenuServiceImpl.java
@@ -10,10 +10,14 @@ import com.mo.common.config.execption.MoException;
 import com.mo.model.system.SysMenu;
 import com.mo.model.system.SysRoleMenu;
 import com.mo.vo.system.AssginMenuVo;
+import com.mo.vo.system.MetaVo;
+import com.mo.vo.system.RouterVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -83,5 +87,94 @@ public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> impl
                 sysRoleMenuService.save(sysRoleMenu);
             }
         }
+    }
+    //根据用户id查询出用户可操作的菜单列表
+    @Override
+    public List<RouterVo> findUserMenuListByUserId(Long userId) {
+        List<SysMenu> sysMenuList = null;
+        //如果是管理员则都可以操作，userId=1是管理员
+        if (userId.longValue() == 1){
+            //查询所有菜单列表
+            LambdaQueryWrapper<SysMenu> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(SysMenu::getStatus,1);
+            wrapper.orderByAsc(SysMenu::getSortValue);
+            sysMenuList = baseMapper.selectList(wrapper);
+        }else {
+            sysMenuList = baseMapper.findMenuListByUserId(userId);
+        }
+        //将查询的菜单构建成路由结构数据
+        //先构建出菜单的树形结构
+        List<SysMenu> sysMenuTreeList = MenuHelper.buildTree(sysMenuList);
+        List<RouterVo> routerList = this.buildRouter(sysMenuTreeList);
+
+        return routerList;
+    }
+
+    private List<RouterVo> buildRouter(List<SysMenu> menus) {
+        List<RouterVo> routers = new ArrayList<>();
+        for (SysMenu menu : menus) {
+            RouterVo router = new RouterVo();
+            router.setHidden(false);
+            router.setAlwaysShow(false);
+            router.setPath(getRouterPath(menu));
+            router.setComponent(menu.getComponent());
+            router.setMeta(new MetaVo(menu.getName(), menu.getIcon()));
+            //下一层数据部分
+            List<SysMenu> children = menu.getChildren();
+            if (menu.getType().intValue() == 1) {
+                //加载隐藏路由
+                List<SysMenu> hiddenMenuList = children.stream().filter(item -> !StringUtils.isEmpty(item.getComponent()))
+                        .collect(Collectors.toList());
+                for (SysMenu hiddenMenu : hiddenMenuList) {
+                    RouterVo hiddenRouter = new RouterVo();
+                    hiddenRouter.setHidden(true);
+                    hiddenRouter.setAlwaysShow(false);
+                    hiddenRouter.setPath(getRouterPath(hiddenMenu));
+                    hiddenRouter.setComponent(hiddenMenu.getComponent());
+                    hiddenRouter.setMeta(new MetaVo(hiddenMenu.getName(), hiddenMenu.getIcon()));
+                    routers.add(hiddenRouter);
+                }
+            }else{
+                if (!CollectionUtils.isEmpty(children)) {
+                    if (children.size() > 0){
+                        router.setAlwaysShow(true);
+                    }
+                    //递归
+                    router.setChildren(buildRouter(children));
+                }
+            }
+            routers.add(router);
+        }
+        return routers;
+    }
+    /**
+     * 获取路由地址
+     *
+     * @param menu 菜单信息
+     * @return 路由地址
+     */
+    public String getRouterPath(SysMenu menu) {
+        String routerPath = "/" + menu.getPath();
+        if(menu.getParentId().intValue() != 0) {
+            routerPath = menu.getPath();
+        }
+        return routerPath;
+    }
+
+    //根据用户id查询出用户可操作的按钮列表
+    @Override
+    public List<String> findUserBtnByUserId(Long userId) {
+        List<SysMenu> sysMenuList = null;
+        //如果是管理员查询所有按钮
+        if (userId.longValue() == 1) {
+            LambdaQueryWrapper<SysMenu> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(SysMenu::getStatus,1);
+            sysMenuList = baseMapper.selectList(wrapper);
+        }else {
+            sysMenuList = baseMapper.findMenuListByUserId(userId);
+        }
+        List<String> btnList = sysMenuList.stream().filter(item -> item.getType() == 2)
+                .map(item -> item.getPerms()).collect(Collectors.toList());
+        return btnList;
     }
 }
